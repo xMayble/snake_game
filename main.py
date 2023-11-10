5dc2d3e9690a932590db9b9e2273dc56530d24c3
Mayble <mehburohit@gmail.com>
2023-11-09 21:46:03 -0500

    creating the snake

--- main.py
+++ main.py
@@ -2,3 +2,26 @@ from turtle import Screen, Turtle
 
 screen = Screen()
 screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+
+x_position = [0,-20,-40]
+
+
+for position in x_position:
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.goto(x=position, y=0)
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
