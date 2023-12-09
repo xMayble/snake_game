a641052ad8b8dda6ae18b1125ac6d409e74042cb
Mayble <mehburohit@gmail.com>
2023-12-09 15:55:54 -0500

    superclass

---
+++ food.py
@@ -0,0 +1,9 @@
+from turtle import Turtle
+
+class Food(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.shapesize(stretch_len=8.5, stretch_wid=8.5)
