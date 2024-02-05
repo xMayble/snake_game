18e2d91980b68113ace26a6d64beb0a06eca36f0
Mehbub Rohit <mehburohit@gmail.com>
2024-02-04 19:01:27 -0500

    collision with wall

--- main.py
+++ main.py
@@ -27,13 +27,19 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
 
+   
+
     # Detect collision with food. 
     if snake.head.distance(food) < 20:
         food.refresh()
         score.increase_score()
-    
 
-   
+     # Detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        score.game_over()
+        
+    
 
 
 
--- scoreboard.py
+++ scoreboard.py
@@ -9,12 +9,16 @@ class Scoreboard(Turtle):
         self.score = 0
         self.color("white")
         self.penup()
-        self.goto(0, 250)
+        self.goto(0, 270)
         self.hideturtle()
         self.update_scoreboard()
 
     def update_scoreboard(self):
         self.write("Score: {}".format(self.score), align=ALIGNMENT, font=FONT)
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
         
     def increase_score(self):
         self.score += 1
